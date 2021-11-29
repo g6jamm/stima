a034132786f7df80639cabbedf2f333f489d1097
Andreas Simmelkiaer <andreas.sim@live.dk>
2021-11-29 11:57:23 +0100

    Working form.
    get input and creates object

--- src/main/java/com/g6jamm/stima/domain/service/TaskService.java
+++ src/main/java/com/g6jamm/stima/domain/service/TaskService.java
@@ -1,3 +1,23 @@
 package com.g6jamm.stima.domain.service;
 
-public class TaskService {}
+import com.g6jamm.stima.data.repository.TaskRepository;
+import com.g6jamm.stima.domain.model.Task;
+
+public class TaskService {
+
+    private TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository){
+        this.taskRepository = taskRepository;
+    }
+
+
+    public Task createtask(String taskName){
+        Task newTask = new Task.TaskBuilder().name(taskName).build();
+
+        return taskRepository.createTask(newTask);
+    }
+
+
+
+}
--- src/main/java/com/g6jamm/stima/web/SubProjectController.java
+++ src/main/java/com/g6jamm/stima/web/SubProjectController.java
@@ -1,12 +1,17 @@
 package com.g6jamm.stima.web;
 
+import com.g6jamm.stima.data.repository.mock.TaskRepositoryStub;
+import com.g6jamm.stima.domain.service.TaskService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.context.request.WebRequest;
 
 @Controller
 public class SubProjectController {
+    
+    TaskService taskService = new TaskService(new TaskRepositoryStub());
 
 
     @GetMapping("/subproject") // TODO rename, skal nok hedde noget andet
@@ -16,9 +21,16 @@ public class SubProjectController {
 
 
     @PostMapping("/create-task")
-    public String createTask(WebRequest webRequest) {
+    public String createTask(WebRequest webRequest, Model model) {
         String name = webRequest.getParameter("task_name");
+        model.addAttribute("Task",taskService.createtask(name));
+        return "Task";
+    }
 
+
+    @GetMapping("/task")
+    public String task(WebRequest webRequest, Model model){
+        model.addAttribute("Task", taskService.createtask("Placeholder"));
         return "Task";
     }
 }
