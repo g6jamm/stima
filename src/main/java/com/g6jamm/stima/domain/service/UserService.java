28a2911f2da33ae6e5dc8b9114191885aab19fe0
Moshizzl3 <Moshizzl3@users.noreply.github.com>
2021-11-29 11:06:57 +0000

    Google Java Format

--- src/main/java/com/g6jamm/stima/domain/service/UserService.java
+++ src/main/java/com/g6jamm/stima/domain/service/UserService.java
@@ -7,22 +7,22 @@ public class UserService {
 
   UserRepository userRepository;
 
-  public UserService(UserRepository userRepository){
+  public UserService(UserRepository userRepository) {
     this.userRepository = userRepository;
   }
 
-  public User login(String email, String password){
+  public User login(String email, String password) {
     return userRepository.login(email, password);
   }
 
-  public User createUser(String firstName, String lastName, String email, String password){
-    User user = new User.UserBuilder()
-        .firstName(firstName)
-        .lastName(lastName)
-        .email(email)
-        .password(password)
-        .build();
+  public User createUser(String firstName, String lastName, String email, String password) {
+    User user =
+        new User.UserBuilder()
+            .firstName(firstName)
+            .lastName(lastName)
+            .email(email)
+            .password(password)
+            .build();
     return userRepository.createUser(user);
   }
-
 }
