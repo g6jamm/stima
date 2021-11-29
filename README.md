fb5a2fe2f1f20a54940141b569a336f8caedd8af
MathiasReker <mathias@reker.dk>
2021-11-29 01:27:27 +0100

    add readme and install script

--- README.md
+++ README.md
@@ -1,2 +1,67 @@
 # stima
-Project estimation tool
+
+![GitHub repo size](https://img.shields.io/github/repo-size/g6jamm/stima)
+![GitHub Issues](https://img.shields.io/github/issues/g6jamm/stima)
+![GitHub contributors](https://img.shields.io/github/contributors/g6jamm/stima)
+![GitHub stars](https://img.shields.io/github/stars/g6jamm/stima)
+![GitHub forks](https://img.shields.io/github/forks/g6jamm/stima)
+![GitHub licence](https://img.shields.io/github/license/g6jamm/stima.svg)
+
+Stima is a project estimation tool.
+
+## Using
+
+To use stima, follow this link: https://stima.herokuapp.com/
+
+## Contributing to wishShare
+
+To contribute to stima, follow these steps:
+
+1. Fork this repository.
+2. Create a branch: `git checkout -b <branch_name>`.
+3. Make your changes and commit them: `git commit -m '<commit_message>'`
+4. Push to the original branch: `git push origin <project_name>/<location>`
+5. Create the pull request.
+
+Alternatively see the GitHub documentation
+on [creating a pull request](https://help.github.com/en/github/collaborating-with-issues-and-pull-requests/creating-a-pull-request)
+.
+
+## Run on localhost
+
+### Step 1 - download:
+
+```
+git clone git@github.com:g6jamm/stima.git && cd stima
+```
+
+### Step 2 - install:
+
+```
+bash install.sh
+```
+
+### Step 3 - build:
+
+```
+mvn install
+```
+
+### Step 4 - run:
+
+```
+mvn spring-boot:run
+```
+
+## Contributors
+
+Thanks to the following people who have contributed to this project:
+
+* [@Andreassim](https://github.com/Andreassim)
+* [@Jarkyman](https://github.com/Jarkyman)
+* [@MathiasReker](https://github.com/MathiasReker)
+* [@Moshizzl3](https://github.com/Moshizzl3)
+
+## License
+
+This project uses the following license: [MIT License](https://github.com/g6jamm/stima/blob/develop/LICENSE).
---
+++ install.sh
@@ -0,0 +1,140 @@
+#!/bin/bash
+
+readonly PROPERTY_FILE="./src/main/resources/application.properties"
+readonly RED="\e[31m"
+readonly GREEN="\e[32m"
+readonly CYAN="\e[36m"
+readonly NC="\e[0m"
+
+run_sql_script() {
+  if [ ! -f "$PROPERTY_FILE" ]; then
+    echo -e "${RED}You must create a property file first${NC}"
+    return
+  fi
+
+  db_localhost=$(get_property_value "url" | cut -d'/' -f3 | cut -d':' -f1)
+  db_port=$(get_property_value "url" | cut -d'/' -f3 | cut -d':' -f2)
+  db_username=$(get_property_value "user")
+  db_password=$(get_property_value "password")
+
+  my_sql_result=$(mysql -B --host="$db_localhost" --port="$db_port" --user="$db_username" --password="$db_password" <"$2" 2>&1 | grep -v "Using a password")
+
+  echo "$1"
+
+  if [ -z "$my_sql_result" ]; then
+    echo -e "${GREEN}Success${NC}"
+  else
+    echo -e "${RED}$my_sql_result${NC}"
+
+  fi
+}
+
+get_property_value() {
+  if [ -f "$PROPERTY_FILE" ]; then
+    (grep -w "$1" | cut -d= -f2) <"$PROPERTY_FILE"
+  fi
+}
+
+set_properties() {
+  # Db localhost
+  get_localhost=$(get_property_value "url" | cut -d'/' -f3 | cut -d':' -f1)
+  if [ -z "$get_localhost" ]; then
+    db_localhost="127.0.0.1"
+  else
+    db_localhost="$get_localhost"
+  fi
+  read -r -e -i "$db_localhost" -p "$(echo -e "${CYAN}Db host: ${NC}")" input
+  db_localhost="${input:-$db_localhost}"
+
+  # Db port
+  get_db_port=$(get_property_value "url" | cut -d'/' -f3 | cut -d':' -f2)
+  if [ -z "$get_db_port" ]; then
+    db_port="3306"
+  else
+    db_port="$get_db_port"
+  fi
+  read -r -e -i "$db_port" -p "$(echo -e "${CYAN}Db port: ${NC}")" input
+  db_port="${input:-$db_port}"
+
+  # Db username
+  get_db_username=$(get_property_value "user")
+  if [ -z "$get_db_username" ]; then
+    db_username="root"
+  else
+    db_username="$get_db_username"
+  fi
+  read -r -e -i "$db_username" -p "$(echo -e "${CYAN}Db username: ${NC}")" input
+  db_username="${input:-$db_username}"
+
+  # Db password
+  get_db_password=$(get_property_value "password")
+  if [ -z "$get_db_password" ]; then
+    db_password="root"
+  else
+    db_password="$get_db_password"
+  fi
+  read -r -e -i "$db_password" -p "$(echo -e "${CYAN}Db password: ${NC}")" input
+  db_password="${input:-$db_password}"
+
+  # Webserver host
+  get_webserver_host=$(get_property_value "baseUrl" | cut -d'/' -f3 | cut -d':' -f1)
+  if [ -z "$get_webserver_host" ]; then
+    webserver_host="127.0.0.1"
+  else
+    webserver_host="$get_webserver_host"
+  fi
+  read -r -e -i "$webserver_host" -p "$(echo -e "${CYAN}Webserver host: ${NC}")" input
+  webserver_host="${input:-$webserver_host}"
+
+  # Webserver port
+  get_webserver_port=$(get_property_value "port")
+  if [ -z "$get_webserver_port" ]; then
+    webserver_port="8080"
+  else
+    webserver_port="$get_webserver_port"
+  fi
+  read -r -e -i "$webserver_port" -p "$(echo -e "${CYAN}Webserver port: ${NC}")" input
+  webserver_port="${input:-$webserver_port}"
+
+  echo "user=$db_username
+password=$db_password
+url=jdbc:mysql://$db_localhost:$db_port/stima
+connection=LOCAL
+port=$webserver_port
+baseUrl=http://$webserver_host:$webserver_port/" >"$PROPERTY_FILE"
+}
+
+# creating a menu
+echo -e "${CYAN}Installation:${NC}"
+echo "1. Set properties"
+echo "2. Install tables"
+echo "3. Install sample data"
+echo "4. Exit"
+echo -n "Enter your menu choice [1-4]: "
+
+while :; do
+  read -r choice
+
+  case $choice in
+  1)
+    set_properties
+    ;;
+
+  2)
+    run_sql_script "Installing database ..." "./src/main/resources/data/install.sql"
+    ;;
+
+  3)
+    run_sql_script "Installing sample data ..." "./src/main/resources/data/sample-data.sql"
+    ;;
+
+  4)
+    echo "Quitting ..."
+    exit
+    ;;
+
+  *) echo "invalid option" ;;
+
+  esac
+  echo -n "Enter your menu choice [1-4]: "
+done
