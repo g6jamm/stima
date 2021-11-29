215148b2e630b439d04438431ea7c2f79a4ecf99
Andreas Simmelkiaer <andreas.sim@live.dk>
2021-11-29 11:29:42 +0100

    Iintial Task.html includes form and some text
    added post mapping to subprojectController

--- src/main/java/com/g6jamm/stima/web/SubProjectController.java
+++ src/main/java/com/g6jamm/stima/web/SubProjectController.java
@@ -2,12 +2,23 @@ package com.g6jamm.stima.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.context.request.WebRequest;
 
 @Controller
 public class SubProjectController {
 
-  @GetMapping("/subproject") // TODO rename, skal nok hedde noget andet
-  public String subProjectPage() {
-    return "subProject";
-  }
+
+    @GetMapping("/subproject") // TODO rename, skal nok hedde noget andet
+    public String subProjectPage() {
+        return "subProject";
+    }
+
+
+    @PostMapping("/create-task")
+    public String createTask(WebRequest webRequest) {
+        String name = webRequest.getParameter("task_name");
+
+        return "Task";
+    }
 }
