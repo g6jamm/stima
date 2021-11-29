40df41719b286abee5382dfe86fae5b8be158c3e
Andreassim <Andreassim@users.noreply.github.com>
2021-11-29 08:31:19 +0000

    Google Java Format

--- src/main/java/com/g6jamm/stima/data/repository/mock/TaskRepositoryStub.java
+++ src/main/java/com/g6jamm/stima/data/repository/mock/TaskRepositoryStub.java
@@ -7,16 +7,17 @@ import com.g6jamm.stima.domain.model.Task;
 import java.time.LocalDate;
 
 public class TaskRepositoryStub implements TaskRepository {
-    @Override
-    public Task createTask(Task task) {
-        task = new Task.TaskBuilder()
-                .name("Test")
-                .role(new Role())
-                .price(1)
-                .hours(1.0)
-                .startDate(LocalDate.of(2021, 1, 1))
-                .endDate(LocalDate.of(2021, 1, 1))
-                .build();
+  @Override
+  public Task createTask(Task task) {
+    task =
+        new Task.TaskBuilder()
+            .name("Test")
+            .role(new Role())
+            .price(1)
+            .hours(1.0)
+            .startDate(LocalDate.of(2021, 1, 1))
+            .endDate(LocalDate.of(2021, 1, 1))
+            .build();
     return task;
   }
 
