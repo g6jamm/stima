cd4662d4df382eaa535923ed7dc2cdd1743365d4
Andreas Simmelkiaer <andreas.sim@live.dk>
2021-11-30 15:06:24 +0100

    Added Static list to SubProject stub
    Getter changed to return new static variable
    
    Constructor to initialize some data

--- src/main/java/com/g6jamm/stima/data/repository/mock/SubProjectRepositoryStub.java
+++ src/main/java/com/g6jamm/stima/data/repository/mock/SubProjectRepositoryStub.java
@@ -10,70 +10,73 @@ import java.util.List;
 
 public class SubProjectRepositoryStub implements SubProjectRepository {
 
-  @Override
-  public List<SubProject> getSubProjects() {
-    List<SubProject> subProjects = new ArrayList<>();
+    private static List<SubProject> subProjects;
 
-    subProjects.add(
-        new SubProject.SubProjectBuilder()
-            .name("example")
-            .hours(100)
-            .price(80000)
-            .startDate(LocalDate.of(2020, 1, 1))
-            .endDate(LocalDate.of(2021, 1, 1))
-            .build());
+    public SubProjectRepositoryStub() {
+        subProjects = new ArrayList<>();
+        subProjects.add(
+                new SubProject.SubProjectBuilder()
+                        .name("example")
+                        .hours(100)
+                        .price(80000)
+                        .startDate(LocalDate.of(2020, 1, 1))
+                        .endDate(LocalDate.of(2021, 1, 1))
+                        .build());
 
-    subProjects.add(
-        new SubProject.SubProjectBuilder()
-            .name("example2")
-            .hours(100)
-            .price(80000)
-            .startDate(LocalDate.of(2020, 1, 1))
-            .endDate(LocalDate.of(2021, 1, 1))
-            .build());
-
-    return subProjects;
-  }
-
-  @Override
-  public SubProject createSubProject(String name, LocalDate startDate, LocalDate endDate) {
-
-    return new SubProject.SubProjectBuilder()
-        .name(name)
-        // .hours(0)
-        // .price(0)
-        // .tasks(null)
-        .startDate(startDate)
-        .endDate(endDate)
-        .build();
-  }
-
-  @Override
-  public SubProject deleteSubProject(SubProject subProject) {
-    return subProject;
-  }
-
-  @Override
-  public double getTotalHours(SubProject subProject) {
-    int result = 0;
-    List<Task> tasks = subProject.getTasks();
-
-    for (Task t : tasks) {
-      result += t.getHours();
+        subProjects.add(
+                new SubProject.SubProjectBuilder()
+                        .name("example2")
+                        .hours(100)
+                        .price(80000)
+                        .startDate(LocalDate.of(2020, 1, 1))
+                        .endDate(LocalDate.of(2021, 1, 1))
+                        .build());
     }
 
-    return result;
-  }
-
-  @Override
-  public int getTotalPrice(SubProject subProject) {
-    int result = 0;
-    List<Task> tasks = subProject.getTasks();
-
-    for (Task t : tasks) {
-      result += t.getPrice();
+    @Override
+    public List<SubProject> getSubProjects() {
+        return subProjects;
     }
 
-    return result;
-  }
+    @Override
+    public SubProject createSubProject(String name, LocalDate startDate, LocalDate endDate) {
+
+        return new SubProject.SubProjectBuilder()
+                .name(name)
+                // .hours(0)
+                // .price(0)
+                // .tasks(null)
+                .startDate(startDate)
+                .endDate(endDate)
+                .build();
+    }
+
+    @Override
+    public SubProject deleteSubProject(SubProject subProject) {
+        return subProject;
+    }
+
+    @Override
+    public double getTotalHours(SubProject subProject) {
+        int result = 0;
+        List<Task> tasks = subProject.getTasks();
+
+        for (Task t : tasks) {
+            result += t.getHours();
+        }
+
+        return result;
+    }
+
+    @Override
+    public int getTotalPrice(SubProject subProject) {
+        int result = 0;
+        List<Task> tasks = subProject.getTasks();
+
+        for (Task t : tasks) {
+            result += t.getPrice();
+        }
+
+        return result;
+    }
 }
