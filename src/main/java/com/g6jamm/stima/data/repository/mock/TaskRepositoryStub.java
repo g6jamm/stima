a71155c4bac697ae25b76978b6f2c0a0ae137af7
Mohamad Ibrahim <mohamedibr@gmail.com>
2021-11-30 11:09:29 +0100

    added list for test

--- src/main/java/com/g6jamm/stima/data/repository/mock/TaskRepositoryStub.java
+++ src/main/java/com/g6jamm/stima/data/repository/mock/TaskRepositoryStub.java
@@ -3,8 +3,16 @@ package com.g6jamm.stima.data.repository.mock;
 import com.g6jamm.stima.data.repository.TaskRepository;
 import com.g6jamm.stima.domain.model.Role;
 import com.g6jamm.stima.domain.model.Task;
+import com.g6jamm.stima.domain.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TaskRepositoryStub implements TaskRepository {
+
+  public static List<Task> taskListStub = new ArrayList<>();
+  private int generatedIdStub = taskListStub.size();
+
   @Override
   public Task createTask(Task task) {
     task =
@@ -17,11 +25,19 @@ public class TaskRepositoryStub implements TaskRepository {
             .endDate(task.getEND_DATE())
             .id(1)
             .build();
+
+    taskListStub.add(task);
     return task;
   }
 
   @Override
   public Task getTask(int task_id) {
-    return null;
+    Task result = null;
+    for (Task t : taskListStub) {
+      if (t.getID() == task_id) {
+        result = t;
+      }
+    }
+    return result;
   }
 }
