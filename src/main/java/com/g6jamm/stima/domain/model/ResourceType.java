0bc3b9ab78a836d3aa01869725ec34d3d4162181
Andreas Simmelkiaer <andreas.sim@live.dk>
2021-12-01 11:33:57 +0100

    resourceTyoe model and builder

---
+++ src/main/java/com/g6jamm/stima/domain/model/ResourceType.java
@@ -0,0 +1,61 @@
+package com.g6jamm.stima.domain.model;
+
+public class ResourceType {
+
+    private final String NAME;
+    private final int PRICE_PR_HOUR;
+    private int ID;
+
+    public ResourceType (ResourceTypeBuilder resourceTypeBuilder) {
+        this.ID = resourceTypeBuilder.id;
+        this.NAME = resourceTypeBuilder.name;
+        this.PRICE_PR_HOUR = resourceTypeBuilder.pricePrHour;
+    }
+
+    public int getId() {
+        return ID;
+    }
+
+    public int getPricePrHour() {
+        return PRICE_PR_HOUR;
+    }
+
+    public String getName() {
+        return NAME;
+    }
+
+    public static class ResourceTypeBuilder{
+        private String name;
+        private int pricePrHour;
+        private int id;
+
+        public ResourceTypeBuilder name(String name){
+            this.name = name;
+            return this;
+        }
+
+        public ResourceTypeBuilder pricePrHour(int pricePrHour){
+            this.pricePrHour = pricePrHour;
+            return this;
+        }
+
+        public ResourceTypeBuilder id(int id){
+            this.id = id;
+            return this;
+        }
+
+        public void reset(){
+            this.name = null;
+            this.id = 0;
+            this.pricePrHour = 0;
+        }
+
+        public ResourceType build(){
+            ResourceType resourceType = new ResourceType(this);
+            reset();
+            return resourceType;
+        }
+
+
+    }
+}
