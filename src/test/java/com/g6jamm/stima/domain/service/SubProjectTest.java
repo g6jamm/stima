4433a95637085aa4eb3201fb89f56c9472fd6df9
Jackie <Jack3459@stud.kea.dk>
2021-12-01 20:11:58 +0100

    added tests

---
+++ src/test/java/com/g6jamm/stima/domain/service/SubProjectTest.java
@@ -0,0 +1,33 @@
+package com.g6jamm.stima.domain.service;
+
+import com.g6jamm.stima.data.repository.stub.SubProjectRepositoryStub;
+import com.g6jamm.stima.domain.model.SubProject;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+public class SubProjectTest {
+
+  @Test
+  public void createRightSubprojectTest() {
+
+  }
+
+  @Test
+  public void createWrongSubProjectTest() {
+
+  }
+
+  @Test
+  public void calcHoursTest() {
+
+  }
+
+  @Test
+  public void calcPriceTest() {
+
+  }
+
+
+}
