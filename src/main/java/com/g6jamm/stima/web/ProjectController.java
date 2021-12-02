9008f3ee1cb79423f0cb4156e1950140f7f6e40b
MathiasReker <mathias@reker.dk>
2021-12-02 15:17:36 +0100

    use fragments

--- src/main/java/com/g6jamm/stima/web/ProjectController.java
+++ src/main/java/com/g6jamm/stima/web/ProjectController.java
@@ -11,7 +11,9 @@ import com.g6jamm.stima.domain.service.SubProjectService;
 import com.g6jamm.stima.domain.service.TaskService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -62,6 +64,7 @@ public class ProjectController {
     Project project = projectService.getProjects().get(0); // TODO: ID of project
 
     model.addAttribute("project", project);
+    model.addAttribute("classActiveSettings", "active");
 
     return "project";
   }
