196a3fce5c3a5eca2599d09e6012c8fed5f7dbae
Mohamad Ibrahim <mohamedibr@gmail.com>
2021-12-06 17:24:14 +0100

    added logic to get tasks method

--- src/main/java/com/g6jamm/stima/data/repository/mysql/TaskRepositoryImpl.java
+++ src/main/java/com/g6jamm/stima/data/repository/mysql/TaskRepositoryImpl.java
@@ -9,6 +9,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 public class TaskRepositoryImpl implements TaskRepository {
@@ -62,6 +63,48 @@ public class TaskRepositoryImpl implements TaskRepository {
 
   @Override
   public List<Task> getTasks(int projectId) {
+    List<Task> result = new ArrayList<>();
+
+    try {
+      String query = "SELECT task_id" +
+          ",t.name as task_name" +
+          ", hours" +
+          ", r.resource_type_id" +
+          ", project_id" +
+          ", start_date" +
+          ", end_date" +
+          ", price_per_hour" +
+          ", r.name as resource_name \n" +
+          "FROM tasks t\n" +
+          "inner join resource_type_id r on t.resource_type_id = r.resource_type_id\n" +
+          "where project_id = ?";
+
+      PreparedStatement ps = DbManager.getInstance().getConnection().prepareStatement(query);
+      ps.setInt(1, projectId);
+      ResultSet resultSet = ps.executeQuery();
+
+      while (resultSet.next()) {
+
+        ResourceType resourceType = new ResourceType.ResourceTypeBuilder()
+            .name(resultSet.getString("resource_name"))
+            .id(resultSet.getInt("resource_type_id"))
+            .pricePrHour(resultSet.getInt("price_per_hour"))
+            .build();
+
+
+        result.add( new Task.TaskBuilder()
+            .id(resultSet.getInt("task_id"))
+            .name(resultSet.getString("task_name"))
+            .startDate(LocalDate.parse(resultSet.getString("start_date")))
+            .endDate(LocalDate.parse(resultSet.getString("end_date")))
+            .hours(resultSet.getDouble("hours"))
+            .resourceType(resourceType)
+            .build());
+      }
+      return result;
+    } catch (SQLException e) {
+      System.out.println(e.getMessage()); // TODO
+    }
     return null;
   }
 }
