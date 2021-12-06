35a1c813be363926d4e2c8ccf149d271efd833b7
Moshizzl3 <Moshizzl3@users.noreply.github.com>
2021-12-06 16:39:48 +0000

    Google Java Format

--- src/main/java/com/g6jamm/stima/data/repository/mysql/TaskRepositoryImpl.java
+++ src/main/java/com/g6jamm/stima/data/repository/mysql/TaskRepositoryImpl.java
@@ -15,38 +15,40 @@ import java.util.List;
 public class TaskRepositoryImpl implements TaskRepository {
   @Override
   public Task createTask(Task task) {
-    String query = "INSERT INTO tasks(name, hours, resource_type, project_id, start_date, end_date)";
-    //where do we get project id from?
+    String query =
+        "INSERT INTO tasks(name, hours, resource_type, project_id, start_date, end_date)";
+    // where do we get project id from?
     return null;
   }
 
   @Override
   public Task getTask(int task_id) {
     try {
-      String query = "SELECT task_id" +
-          ",t.name as task_name" +
-          ", hours" +
-          ", r.resource_type_id" +
-          ", project_id" +
-          ", start_date" +
-          ", end_date" +
-          ", price_per_hour" +
-          ", r.name as resource_name \n" +
-          "FROM tasks t\n" +
-          "inner join resource_type_id r on t.resource_type_id = r.resource_type_id\n" +
-          "where task_id = ?";
+      String query =
+          "SELECT task_id"
+              + ",t.name as task_name"
+              + ", hours"
+              + ", r.resource_type_id"
+              + ", project_id"
+              + ", start_date"
+              + ", end_date"
+              + ", price_per_hour"
+              + ", r.name as resource_name \n"
+              + "FROM tasks t\n"
+              + "inner join resource_type_id r on t.resource_type_id = r.resource_type_id\n"
+              + "where task_id = ?";
       PreparedStatement ps = DbManager.getInstance().getConnection().prepareStatement(query);
       ps.setInt(1, task_id);
       ResultSet resultSet = ps.executeQuery();
 
       if (resultSet.next()) {
 
-        ResourceType resourceType = new ResourceType.ResourceTypeBuilder()
-            .name(resultSet.getString("resource_name"))
-            .id(resultSet.getInt("resource_type_id"))
-            .pricePrHour(resultSet.getInt("price_per_hour"))
-            .build();
-
+        ResourceType resourceType =
+            new ResourceType.ResourceTypeBuilder()
+                .name(resultSet.getString("resource_name"))
+                .id(resultSet.getInt("resource_type_id"))
+                .pricePrHour(resultSet.getInt("price_per_hour"))
+                .build();
 
         return new Task.TaskBuilder()
             .id(resultSet.getInt("task_id"))
@@ -68,18 +70,19 @@ public class TaskRepositoryImpl implements TaskRepository {
     List<Task> result = new ArrayList<>();
 
     try {
-      String query = "SELECT task_id" +
-          ",t.name as task_name" +
-          ", hours" +
-          ", r.resource_type_id" +
-          ", project_id" +
-          ", start_date" +
-          ", end_date" +
-          ", price_per_hour" +
-          ", r.name as resource_name \n" +
-          "FROM tasks t\n" +
-          "inner join resource_type_id r on t.resource_type_id = r.resource_type_id\n" +
-          "where project_id = ?";
+      String query =
+          "SELECT task_id"
+              + ",t.name as task_name"
+              + ", hours"
+              + ", r.resource_type_id"
+              + ", project_id"
+              + ", start_date"
+              + ", end_date"
+              + ", price_per_hour"
+              + ", r.name as resource_name \n"
+              + "FROM tasks t\n"
+              + "inner join resource_type_id r on t.resource_type_id = r.resource_type_id\n"
+              + "where project_id = ?";
 
       PreparedStatement ps = DbManager.getInstance().getConnection().prepareStatement(query);
       ps.setInt(1, projectId);
@@ -87,21 +90,22 @@ public class TaskRepositoryImpl implements TaskRepository {
 
       while (resultSet.next()) {
 
-        ResourceType resourceType = new ResourceType.ResourceTypeBuilder()
-            .name(resultSet.getString("resource_name"))
-            .id(resultSet.getInt("resource_type_id"))
-            .pricePrHour(resultSet.getInt("price_per_hour"))
-            .build();
+        ResourceType resourceType =
+            new ResourceType.ResourceTypeBuilder()
+                .name(resultSet.getString("resource_name"))
+                .id(resultSet.getInt("resource_type_id"))
+                .pricePrHour(resultSet.getInt("price_per_hour"))
+                .build();
 
-
-        result.add( new Task.TaskBuilder()
-            .id(resultSet.getInt("task_id"))
-            .name(resultSet.getString("task_name"))
-            .startDate(LocalDate.parse(resultSet.getString("start_date")))
-            .endDate(LocalDate.parse(resultSet.getString("end_date")))
-            .hours(resultSet.getDouble("hours"))
-            .resourceType(resourceType)
-            .build());
+        result.add(
+            new Task.TaskBuilder()
+                .id(resultSet.getInt("task_id"))
+                .name(resultSet.getString("task_name"))
+                .startDate(LocalDate.parse(resultSet.getString("start_date")))
+                .endDate(LocalDate.parse(resultSet.getString("end_date")))
+                .hours(resultSet.getDouble("hours"))
+                .resourceType(resourceType)
+                .build());
       }
       return result;
     } catch (SQLException e) {
