9b911bb31e7a15e892d2de1d01e97543170d7e71
Andreas Simmelkiaer <andreas.sim@live.dk>
2021-12-16 22:58:17 +0100

    javadocs

--- src/main/java/com/g6jamm/stima/domain/service/ProjectService.java
+++ src/main/java/com/g6jamm/stima/domain/service/ProjectService.java
@@ -10,63 +10,89 @@ import java.util.List;
 
 public class ProjectService {
 
-  private final ProjectRepository PROJECT_REPOSITORY;
+    private final ProjectRepository PROJECT_REPOSITORY;
 
-  public ProjectService(ProjectRepository projectRepository) {
-    this.PROJECT_REPOSITORY = projectRepository;
-  }
-
-  /** @auther Mathias */
-  public List<Headproject> getProjects(User user) throws SystemException {
-    return PROJECT_REPOSITORY.getProjects(user);
-  }
-
-  /** @auther Mathias */
-  public Headproject getProjectById(User user, int projectID) throws SystemException {
-    for (Headproject project : getProjects(user)) {
-      if (projectID == project.getId()) {
-        return project;
-      }
+    public ProjectService(ProjectRepository projectRepository) {
+        this.PROJECT_REPOSITORY = projectRepository;
     }
 
-    return null;
-  }
+    /**
+     * Method for getting a list of projects based on the user.
+     * This forwards the request to the repository.
+     *
+     * @author Mathias
+     */
+    public List<Headproject> getProjects(User user) throws SystemException {
+        return PROJECT_REPOSITORY.getProjects(user);
+    }
 
-  /** @auther Mathias */
-  public Headproject createProject(
-      String name, LocalDate startDate, LocalDate endDate, String projectColor, User user)
-      throws SystemException {
+    /**
+     * Method for getting a project based on id and the user.
+     * This gets all projects for the user and loops through the list a check if the id exists.
+     *
+     * @author Mathias
+     */
+    public Headproject getProjectById(User user, int projectID) throws SystemException {
+        for (Headproject project : getProjects(user)) {
+            if (projectID == project.getId()) {
+                return project;
+            }
+        }
 
-    Headproject project =
-        new Headproject.ProjectBuilder()
-            .projectName(name)
-            .startDate(startDate)
-            .endDate(endDate)
-            .colorCode(projectColor)
-            .build();
+        return null;
+    }
 
-    return PROJECT_REPOSITORY.createProject(project, user);
-  }
+    /**
+     * Method for creating a new headproject Here we create an initial object for us to use in the repository.
+     * The new object is then passed to the repository to create the rest of the object.
+     * <p>
+     * a complete object with id is then returned.
+     *
+     * @author Mathias
+     */
+    public Headproject createProject(
+            String name, LocalDate startDate, LocalDate endDate, String projectColor, User user)
+            throws SystemException {
 
-  /** @auther Mathias */
-  public void editProject(
-      int projectId, String name, LocalDate startDate, LocalDate endDate, String projectColor)
-      throws SystemException {
+        Headproject project =
+                new Headproject.ProjectBuilder()
+                        .projectName(name)
+                        .startDate(startDate)
+                        .endDate(endDate)
+                        .colorCode(projectColor)
+                        .build();
 
-    Headproject project =
-        new Headproject.ProjectBuilder()
-            .projectId(projectId)
-            .projectName(name)
-            .startDate(startDate)
-            .endDate(endDate)
-            .colorCode(projectColor)
-            .build();
+        return PROJECT_REPOSITORY.createProject(project, user);
+    }
 
-    PROJECT_REPOSITORY.editProject(project);
-  }
+    /**
+     * Method for editing a project.
+     * A new object is build here with the new information. This headproject is then passed to the repository.
+     *
+     * @author Mathias
+     */
+    public void editProject(
+            int projectId, String name, LocalDate startDate, LocalDate endDate, String projectColor)
+            throws SystemException {
 
-  /** @auther Mathias */
-  public void deleteProject(int projectId) throws SystemException {
-    PROJECT_REPOSITORY.deleteProject(projectId);
-  }
+        Headproject project =
+                new Headproject.ProjectBuilder()
+                        .projectId(projectId)
+                        .projectName(name)
+                        .startDate(startDate)
+                        .endDate(endDate)
+                        .colorCode(projectColor)
+                        .build();
+
+        PROJECT_REPOSITORY.editProject(project);
+    }
+
+    /**
+     * Method for forwading the delete request to the repository.
+     *
+     * @author Mathias
+     */
+    public void deleteProject(int projectId) throws SystemException {
+        PROJECT_REPOSITORY.deleteProject(projectId);
+    }
 }
