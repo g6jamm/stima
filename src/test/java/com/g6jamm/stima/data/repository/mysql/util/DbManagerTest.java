1e078bc0b2cda9dbcdf2b8e8425b85d45129a0bd
MathiasReker <mathias@reker.dk>
2021-12-16 23:56:12 +0100

    fix import

--- src/test/java/com/g6jamm/stima/data/repository/mysql/util/DbManagerTest.java
+++ src/test/java/com/g6jamm/stima/data/repository/mysql/util/DbManagerTest.java
@@ -3,12 +3,15 @@ package com.g6jamm.stima.data.repository.mysql.util;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.sql.Connection;
+
 class DbManagerTest {
 
   @Test
   void testConnection() {
 
-    java.sql.Connection result = DbManager.getInstance().getConnection();
+    Connection result = DbManager.getInstance().getConnection();
+
     Assertions.assertNotNull(result);
   }
 }
