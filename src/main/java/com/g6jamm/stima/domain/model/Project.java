3608f848603181ac9b0fc49a2fc1cbe605d3ef28
Andreassim <Andreassim@users.noreply.github.com>
2021-12-16 13:08:23 +0000

    Google Java Format

--- src/main/java/com/g6jamm/stima/domain/model/Project.java
+++ src/main/java/com/g6jamm/stima/domain/model/Project.java
@@ -3,13 +3,11 @@ package com.g6jamm.stima.domain.model;
 import java.time.LocalDate;
 import java.util.List;
 
-/** Interface for project,
- *  Here we use a design Pattern called Composite Pattern to handle both HeadProject and Subproject the same.
- *  There by creating polymophic behaviour and saving code.
- *
- * @Author Andreas
+/**
+ * Interface for project, Here we use a design Pattern called Composite Pattern to handle both
+ * HeadProject and Subproject the same. There by creating polymophic behaviour and saving
+ * code. @Author Andreas
  */
-
 public interface Project {
 
   List<Task> getTasks();
