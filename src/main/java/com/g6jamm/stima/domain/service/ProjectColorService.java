f424e75c60605aad32a433ea07d47b3cab9bbd7c
MathiasReker <mathias@reker.dk>
2021-12-16 23:59:54 +0100

    fix naming

--- src/main/java/com/g6jamm/stima/domain/service/ProjectColorService.java
+++ src/main/java/com/g6jamm/stima/domain/service/ProjectColorService.java
@@ -6,15 +6,15 @@ import com.g6jamm.stima.domain.exception.SystemException;
 import java.util.Map;
 
 public class ProjectColorService {
-  private final ProjectColorRepository PROJECT_COLOR_REPOSITORY_STUB;
+  private final ProjectColorRepository PROJECT_COLOR_REPOSITORY;
 
   /** @auther Mathias */
   public ProjectColorService(ProjectColorRepository projectColorRepositoryStub) {
-    this.PROJECT_COLOR_REPOSITORY_STUB = projectColorRepositoryStub;
+    this.PROJECT_COLOR_REPOSITORY = projectColorRepositoryStub;
   }
 
   /** @auther Mathias */
   public Map<String, String> getProjectColors() throws SystemException {
-    return PROJECT_COLOR_REPOSITORY_STUB.getProjectColors();
+    return PROJECT_COLOR_REPOSITORY.getProjectColors();
   }
 }
